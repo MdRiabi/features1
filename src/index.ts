e0733649222ea6847a734e9074b1043f8d57d54b
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-12-10 13:32:16 +0100

    add a custom  marker place method for user and company

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,17 @@
-/* import { Company } from './Company';
+import { CustomMap } from "./CustomMap";
+ import { Company } from './Company'; 
 import {User} from './User';
 
 
+
+
+
+
+
 const user = new User();
+const comp = new Company();
+const cm =  new CustomMap('map');
 
-console.log(user);
-
-const company = new Company();
-console.log(company);
- */
-
-
-
-
-
-
-const map = new google.maps.Map(document.getElementById("map")as HTMLElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+cm.addMarker(user);
+cm.addMarker(comp);
 
